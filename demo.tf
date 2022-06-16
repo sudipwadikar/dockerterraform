7c4b79f98eb54de9c8cf6816e54d1c425c9e0061
sudipwadikar <107179469+sudipwadikar@users.noreply.github.com>
2022-06-16 15:11:11 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -171,28 +171,28 @@ resource "aws_instance" "demoinstance1" {
   }
   
   # SSH into instance 
-  connection {
+  #connection {
     # The default username for our AMI
-    user = "ec2-user"
+  #  user = "ec2-user"
     # Private key for connection
-    private_key = "${file(var.private_key)}"
+  #  private_key = "${file(var.private_key)}"
     # Type of connection
-    type = "ssh"
-  }
+  #  type = "ssh"
+  #}
   
   # Installing splunk & creating distributed indexer clustering on newly created instance
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo amazon-linux-extras install docker -y",
-      "sudo service docker start",
-      "sudo usermod -a -G docker ec2-user",
-      "sudo chkconfig docker on",
-      "sudo yum install -y git",
-      "sudo chmod 666 /var/run/docker.sock",
-      "docker pull ubuntu",
-      "docker run -d ubuntu sleep 300"   
-  ]
+  #provisioner "remote-exec" {
+  #  inline = [
+  #    "sudo yum update -y",
+  #    "sudo amazon-linux-extras install docker -y",
+  #    "sudo service docker start",
+  #    "sudo usermod -a -G docker ec2-user",
+  #    "sudo chkconfig docker on",
+  #    "sudo yum install -y git",
+  #    "sudo chmod 666 /var/run/docker.sock",
+  #    "docker pull ubuntu",
+  #    "docker run -d ubuntu sleep 300"   
+  #]
  }
 }
 
