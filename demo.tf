14a13301dcaeb36c8113ccc44b11b52c1c00ef08
sudipwadikar <107179469+sudipwadikar@users.noreply.github.com>
2022-06-16 14:35:56 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -190,8 +190,8 @@ resource "aws_instance" "demoinstance1" {
       "sudo chkconfig docker on",
       "sudo yum install -y git",
       "sudo chmod 666 /var/run/docker.sock",
-      "docker pull dhruvin30/dhsoniweb:v1",
-      "docker run -d -p 80:80 dhruvin30/dhsoniweb:v1"   
+      "docker pull ubuntu",
+      "docker run -d ubuntu sleep 300"   
   ]
  }
 }
